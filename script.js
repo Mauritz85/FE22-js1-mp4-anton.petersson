9c785b2f42387a0e16747dfa6164cf1118a573cc
Mauritz85 <109244116+Mauritz85@users.noreply.github.com>
2023-01-09 00:01:05 +0100

    Error-fix
    Fixed error when number of images found are lower than requested.

--- script.js
+++ script.js
@@ -1,176 +1,98 @@
-let spotlightPath = anime.path('#svg path')
+const textInput = document.getElementById('text-input')
+const sizeInput = document.getElementById('size-input')
+const sortInput = document.getElementById('sort-input')
+const amountInput = document.getElementById('amount-input')
+const btn = document.getElementById('search-button')
+const imgElement = document.createElement('img')
+const searchResultDiv = document.getElementById('search-result-div')
 
-const spotlightMovement = anime({
-  targets: '.spotlight',
-  translateX: spotlightPath('x'),
-  translateY: spotlightPath('y'),
-  rotate: spotlightPath('angle'),
-  easing: 'linear',
-  duration: 10000,
-  loop: true,
-  autoplay: false,
-  direction: 'alternate',
-  
-});
-
-const spotlightColorAnimation = anime({
-  targets: '.spotlight',
-  backgroundColor: 'rgb(100, 105, 200)',
-  duration: 200,
-  loop: true,
-  direction: 'alternate',
-  easing: 'easeInOutExpo'
-  
+const searchAnimation = anime({
+    targets: '#green-box',
+    duration: 600,
+    rotate: '360deg',
+    backgroundColor: '#FFF',
+    borderRadius: '50%',
+    easing: 'easeInOutQuad',
+    autoplay: false,
+    loop: true,
+    direction: 'alternate'
 
 });
 
-const spotlightOpacityAnimation = anime({
-  targets: '.spotlight',
-  backgroundColor: 'rgb(100, 105, 200)',
-  duration: 1700,
-  loop: true,
-  direction: 'alternate',
-  opacity: 0.8,
-  autoplay: false,
-  easing: 'easeInOutExpo'
-  
+btn.addEventListener('click', searchFunction)
 
-});
+function searchFunction(event) {
+    event.preventDefault()
+    searchResultDiv.innerHTML = '';
+    if (textInput.value == '') {
+        errorAlertTextInput()
+    }
+    else if (amountInput.value > 99 || amountInput.value < 1) {
+        errorAlertAmountInput()
+    }
 
 
-const wooferAnimation = anime ({
-  targets: '.woofer',
-  duration: 1000,
-  loop: true,
-  duration: 450,
-  autoplay: false,
-  backgroundColor: 'rgb(90, 90, 90)'
+    else {
+        searchAnimation.play()
+        fetch('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=08ec57d2da226c052d2dd04d01cd30fe&text=' + textInput.value + '&sort=' + sortInput.value + '&format=json&nojsoncallback=1')
 
-});
+            .then(response => {
 
-const wooferCentreAnimation = anime({
-  targets: '.woofer-centre',
-  scale: 1.5,
-  loop: true,
-  duration: 450,
-  autoplay: false
-});
+                if (response.ok) {
+                    return response.json()
+                }
+                else {
+                    errorFetchFailed()
+                }
 
-function getRandom() {
-  return Math.random() * 100;
+            })
+            .then(showImages)
+            .catch(errorFetchFailed)
+    }
 }
 
-
-const notesAnimation1 = anime ({
-  targets: '.note1',
-  loop: true,
-  easing: 'easeInOutSine',
-  color: 'rgb(400, 200, 200)',
-  duration: 1000,
-  fontSize: '50px',
-  loopBegin: function getRandom(anim) {
-    Math.random() * 100
-  },
-  translateX: getRandom(),
-  translateY: getRandom(),
-  autoplay: false
-});
-
-const notesAnimation2 = anime({
-  targets: '.note2',
-  loop: true,
-  easing: 'easeInOutSine',
-  color: 'rgb(255, 238, 3)',
-  duration: 1000,
-  fontSize: '50px',
-  loopBegin: function getRandom(anim) {
-    Math.random() * -100
-  },
-  translateX: getRandom(),
-  translateY: getRandom(),
-  delay: 500,
-  autoplay: false
-});
-
-const notesAnimation3 = anime({
-  targets: '.note3',
-  loop: true,
-  easing: 'easeInOutSine',
-  color: 'rgb(200, 238, 3)',
-  duration: 1000,
-  fontSize: '50px',
-  loopBegin: function getRandom(anim) {
-    Math.random() * 100
-  },
-  translateX: getRandom(),
-  translateY: getRandom(),
-  delay: 1000,
-  autoplay: false
-});
-
-const notesAnimation4 = anime({
-  targets: '.note4',
-  loop: true,
-  easing: 'easeInOutSine',
-  color: 'rgb(200, 100, 3)',
-  duration: 1000,
-  fontSize: '50px',
-  loopBegin: function getRandom(anim) {
-    Math.random() * 100
-  },
-  translateX: getRandom(),
-  translateY: getRandom(),
-  delay: 1500,
-  autoplay: false,
-
-});
-
-const backgroundAnimation = anime({
-  targets: '.speaker-box',
-  backgroundColor: '#000000',
-  duration: 500,
-  loop: true,
-  direction: 'reverse',
-  autoplay: false,
-});
-
-const playBtn = document.getElementById('play')
-const pauseBtn = document.getElementById('pause')
-const stopBtn = document.getElementById('stop')
+function showImages(flickrData) {
+    if (flickrData.photos.total === 0) {
+        errorNothingFound()
+    }
 
 
-const animationsArray =[ 
-  spotlightMovement,
-  spotlightOpacityAnimation,
-  wooferAnimation,
-  wooferCentreAnimation,
-  notesAnimation1,
-  notesAnimation2,
-  notesAnimation3,
-  notesAnimation4,
-  backgroundAnimation
-];
+    else {
+        if (flickrData.photos.total < amountInput.value){
+            amountInput.value = flickrData.photos.total
+            errorLesserImagesFound()
+        }
+        for (let i = 0; i < amountInput.value; i++) {
+            imgElement[i] = document.createElement('img')
+            imgElement[i].src = 'https://live.staticflickr.com/' + flickrData.photos.photo[i].server + '/' + flickrData.photos.photo[i].id + '_' + flickrData.photos.photo[i].secret + '_' + sizeInput.value + '.jpg'
+            searchResultDiv.appendChild(imgElement[i])
+        }
+        searchAnimation.restart()
+        searchAnimation.pause()
+    }
+}
 
+function errorAlertTextInput() {
+    alert("Keyword can't be blank.");
+    location.reload()
+}
 
-playBtn.addEventListener('click', function() {
-  for (let i=0; i<animationsArray.length; i++){
-    animationsArray[i].play()
+function errorAlertAmountInput() {
+    alert("Number of images has to be a number between 1 and 99.");
+    location.reload()
+}
 
-  }
-});
+function errorNothingFound() {
+    alert("Nothing found. Try another keyword.");
+    location.reload()
+}
 
-pauseBtn.addEventListener('click', function() {
-  for (let i=0; i<animationsArray.length; i++){
-    animationsArray[i].pause()
+function errorFetchFailed() {
+    alert("Error. Data not available.");
+    location.reload()
+}
 
-  }
-});
-
-stopBtn.addEventListener('click', function() {
-  for (let i=0; i<animationsArray.length; i++){
-    animationsArray[i].restart()
-    animationsArray[i].pause()
-
-  }
-});
+function errorLesserImagesFound(){
+    alert("Sorry. We couldn't find the requested number of images to match your keyword. All available matching images are shown instead.");
+}
 
